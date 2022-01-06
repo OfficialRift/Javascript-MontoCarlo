ccedf51405ee3b0452887ad4c0ae4cb70e1c3062
Sebastian Stenbom <ryezze@gmail.com>
2022-01-06 17:17:00 +0100

    add code

---
+++ Calc.js
@@ -0,0 +1,15 @@
+var points_total = 0;
+var points_inside = 0;
+
+for(var i = 0; i< 100000; i++){
+  points_total++;
+
+  var x = Math.random();
+  var y = Math.random();
+
+  if(Math.pow(x, 2) + Math.pow(y, 2) < 1)
+    points_inside++;
+
+  //if(points_total % 10000 == 0)
+    console.log(" pi == " + (4 * points_inside / points_total));
+}
