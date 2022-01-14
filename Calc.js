012ed7357ad421fbaa28505ef3eedf92dc1f4230
Sebastian Stenbom <ryezze@gmail.com>
2022-01-14 23:18:49 +0100

    fix

--- Calc.js
+++ Calc.js
@@ -7,7 +7,8 @@ for(var i = 0; i< 1000000; i++){
   var x = Math.random();
   var y = Math.random();
 
-  if(Math.pow(x, 2) + Math.pow(y, 2) < 1)
+  if(Math.pow(x, 2) + Math.pow(y, 2) <= 1){
     points_inside++;
+  }
   console.log(" pi = " + (4 * points_inside / points_total));
 }
